5abbc9286fa0e89ac92f114203a81c96e1abf743
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-01-14 04:03:28 +0530

    next Postgres

---
+++ src/main/java/com/example/restapi_nerzon/RestApiNerzonApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapi_nerzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiNerzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiNerzonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapi_nerzon/RestApiNerzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapi_nerzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiNerzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
